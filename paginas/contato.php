951121c4cf80f773b731ed342531303d7c1d1e95
Matheus Santos <36865870+tchiteu@users.noreply.github.com>
2019-05-09 14:59:06 -0300

    Commit Inicial.

---
+++ paginas/contato.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<title>PERTER PAN</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+
+<link rel="stylesheet" href="../css/style.css">
+
+<script src="javascript/script.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+<link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+    
+</head>
+<body>
+
+<header class="cabecalho">
+    <img class="logo" src="../imagens/logo.png" alt="">
+    <ul>
+        <a class="ativo"href="contato.php">Contato</a>
+        <a href="sobre.php">Quem Somos</a>
+        <a href="../index.php">Inicio</a>
+    </ul>
+</header>
+
+<div class="rodape">
+    <div>
+        <i class="fas fa-phone"></i>
+        &nbsp
+        (47) 3288-9050 
+        &nbsp 
+        (47) 99985-2553  
+
+        &nbsp &nbsp &nbsp &nbsp
+        <i class="fas fa-envelope"></i>
+        &nbsp 
+        recepcaopeterpan@gmail.com
+
+        &nbsp &nbsp &nbsp &nbsp
+        <i class="fas fa-map-marker-alt"></i>
+        &nbsp 
+        Rua Pandiá Calógeras, 38 Jardim Blumenau, Blumenau/SC
+    </div>
+
+</body>
+</html>
+
